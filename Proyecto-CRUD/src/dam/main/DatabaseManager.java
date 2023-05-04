2e22cb8131e4e29e7022ccb66dcdfc56429de8c7
Toni <tonicrespomengual@gmail.com>
2023-05-04 10:49:22 +0200

    Añado metodos a la tabla manga donde puedo filtar por algunos parametros

--- Proyecto-CRUD/src/dam/main/DatabaseManager.java
+++ Proyecto-CRUD/src/dam/main/DatabaseManager.java
@@ -15,8 +15,15 @@ import org.eclipse.jdt.annotation.NonNull;
 public class DatabaseManager {
 	private Connection connection = null;
 	private Statement statement = null;
-	private boolean mangaUpdated = false;
-	private ArrayList<Manga> mangaData;
+	//private boolean mangaUpdated = false;
+	//private ArrayList<Manga> mangaData;
+	
+	// Acceso a tablas
+	public static int MANGA = 1, AUTOR = 2, EDITORIAL = 3;
+	
+	// Atributos tabla Manga
+	//public static int ID_manga = 1; 
+	
 	/**
 	 * Constructor especializado en inicializar objetos
 	 * de tipo DatabaseManager a partir de un objeto de conexión
@@ -27,12 +34,15 @@ public class DatabaseManager {
 		this.connection = connection;
 		try {
 			this.statement = connection.createStatement();
-			this.mangaData = new ArrayList<Manga>();
+			//this.mangaData = new ArrayList<Manga>();
 		} catch (SQLException e) {			
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Metodos Manga
+	 */
 	public ArrayList<Manga> getManga(){
 		ArrayList<Manga> manga = null;
 		try {
@@ -56,6 +66,71 @@ public class DatabaseManager {
 		return manga;
 	}
 	
+	/**
+	 * Metodo para buscar manga por id
+	 * @param id 
+	 * @return
+	 */
+	public ArrayList<Manga> getManga(int id){
+		ArrayList<Manga> manga = null;
+		try {
+			PreparedStatement ps = this.connection.
+					prepareStatement("SELECT * FROM manga WHERE id = ?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			manga = new ArrayList<Manga>();
+			while(rs.next()) {
+				manga.add(new Manga(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						rs.getDate(5).toLocalDate(),
+						rs.getInt(6),
+						rs.getInt(7)));
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return manga;
+	}
+	
+	/**
+	 * Método para buscar manga por id de autor y editorial.
+	 * Es una búsqueda de AND.
+	 * @param id_autor
+	 * @param id_editorial
+	 * @return
+	 */
+	public ArrayList<Manga> getManga(int id_autor, int id_editorial){
+		ArrayList<Manga> manga = null;
+		try {
+			PreparedStatement ps = this.connection.
+					prepareStatement("SELECT * FROM manga "
+							+ "WHERE id_autor = ? AND id_editorial = ?");
+			ps.setInt(1, id_autor);
+			ps.setInt(2, id_editorial);
+			ResultSet rs = ps.executeQuery();
+			manga = new ArrayList<Manga>();
+			while(rs.next()) {
+				manga.add(new Manga(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						rs.getDate(5).toLocalDate(),
+						rs.getInt(6),
+						rs.getInt(7)));
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return manga;
+	}
+	
+	/**
+	 * Metodos Autor
+	 */
 	public ArrayList<Autor> getAutor(){
 		ArrayList<Autor> autor = null;
 		try {
@@ -77,6 +152,9 @@ public class DatabaseManager {
 		return autor;
 	}
 	
+	/**
+	 * Metodos Editorial
+	 */
 	public ArrayList<Editorial> getEditorial(){
 		ArrayList<Editorial> editorial = null;
 		try {
