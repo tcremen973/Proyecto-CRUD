fb0ff79fe5d9c239484a0ce56850aef6a69c7948
Toni <tonicrespomengual@gmail.com>
2023-05-22 15:59:29 +0200

    Ignorado clase Main

--- Proyecto-CRUD/src/dam/main/Main.java
+++ Proyecto-CRUD/src/dam/main/Main.java
@@ -2,6 +2,8 @@ package dam.main;
 
 import java.util.ArrayList;
 
+import org.w3c.dom.Document;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,9 +13,21 @@ public class Main {
 		DatabaseManager databaseManager = new DatabaseManager(connection);
 		
 		ArrayList<Elemento> tabla = databaseManager.getTabla(DatabaseManager.MANGA);
+		tabla.addAll(databaseManager.getTabla(DatabaseManager.AUTOR));
+		tabla.addAll(databaseManager.getTabla(DatabaseManager.EDITORIAL));
 		
 		for (Elemento elemento : tabla) {
 			System.out.println(elemento);
 		}
+		
+		Document doc = databaseManager.exportarTabla(tabla, "/home/usuario/salidaBBDD/TablaExportada.xml");
+		tabla = databaseManager.importarXml("/home/usuario/salidaBBDD/TablaExportada.xml");
+		
+		System.out.println("\n Importacion");
+		for (Elemento elemento : tabla) {
+			System.out.println(elemento);
+		}
+		
+		System.out.println(databaseManager.comprobarIntegridad());
 	}
 }
